37c16161085adf19d48b0735642316c934b09997
Bill Mei <billmei@users.noreply.github.com>
2013-04-01 23:29:33 -0400

    JProgressBar params setup

--- LoadingBarTimer.java
+++ LoadingBarTimer.java
@@ -1,36 +1,40 @@
+import java.util.Date;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class LoadingBarTimer extends JFrame {
 
-    private static final int TIME_WARP = 1; // e.g. 2 = 2x speed
     private static final int WIDTH = 200;
     private static final int HEIGHT = 25;
     private MainPanel mainPanel = new MainPanel();
-    private TimerListener clockCounter = new TimerListener();
-    private Timer timer = new Timer(1000 / TIME_WARP, clockCounter);
-    private long theCurrentTime = System.currentTimeMillis();
-    private JProgressBar theBar = new JProgressBar();
+    private Timer globalTimer = new Timer(1000, new TimerListener()); // updates every second
+    private JProgressBar theBar = new JProgressBar(0, 1); // maximum value is overridden later
 
     public LoadingBarTimer () {
-        long numberOfHours = Long.parseLong(JOptionPane.showInputDialog(null, "Input the number of hours:"));
-        long numberOfMinutes = Long.parseLong(JOptionPane.showInputDialog(null, "Input the number of minutes:"));
-        long numberOfSeconds = (numberOfMinutes * 60) + (numberOfHours * 3600);
+        int numberOfHours = Integer.parseInt(JOptionPane.showInputDialog(null, "Input the number of hours:"));
+        int numberOfMinutes = Integer.parseInt(JOptionPane.showInputDialog(null, "Input the number of minutes:"));
+        int numberOfSeconds = (numberOfMinutes * 60) + (numberOfHours * 3600);
 
-        long timeYouStarted = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + (1000 * numberOfSeconds);
+        int timeDelta = (int) (endTime - startTime); // milliseconds
 
-        theBar.setMaximum((int) (timeYouStarted + numberOfSeconds));
+        theBar.setMaximum(timeDelta);
+        theBar.setStringPainted(true);
 
-        timer.addActionListener(clockCounter);
+        System.out.println(startTime);
+        System.out.println(endTime);
+        System.out.println(timeDelta);
 
-        add(mainPanel, BorderLayout.CENTER);
+        add(mainPanel);
+        mainPanel.setLayout(new GridLayout(0,1)); // gridlayout fills out the entire window
         mainPanel.add(theBar);
-
-        System.out.println(theCurrentTime);
-
         mainPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 
+        globalTimer.start();
+
+        setAlwaysOnTop(true);
         setTitle("Timer");
         setDefaultCloseOperation(EXIT_ON_CLOSE); 
         pack(); 
@@ -39,8 +43,13 @@ public class LoadingBarTimer extends JFrame {
 
     public class TimerListener implements ActionListener {
         public void actionPerformed (ActionEvent e) {
-            theBar.setValue((int) theCurrentTime*1000);
+            long currentTime = System.currentTimeMillis();
+            System.out.println(currentTime);
             repaint();
+        }
+
+        public int returnNewTimeDelta () {
+            return 2; // placeholder
         }
     }
 
